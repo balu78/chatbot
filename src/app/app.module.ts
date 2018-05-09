4cb41c3dea7a380a6f4615d88f7069bd42d8c7bf
balu78 <chbrammohan@gmail.com>
2018-05-09 15:59:10 +0530

    UI/UX changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
+import { environment } from '../environments/environment.prod';
 
 import { AppComponent } from './app.component';
-
-
+import { ChatDialogComponent } from './chat/chat-dialog/chat-dialog.component';
+import { ChatModule } from './chat/chat.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    ChatModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
